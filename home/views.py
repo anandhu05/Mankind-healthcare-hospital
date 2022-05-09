a50bb6f8d995ae65271a5b3f6973557ec17f4f50
Anandhu Biju <anandhubijuabk7598@gmail.com>
2022-05-09 09:22:25 +0530

    Templates added

--- home/views.py
+++ home/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+  
+
+def about(request):
+     return render(request, 'about.html')
+
+def booking(request):
+
+    return render(request, 'booking.html')
+
+def doctor(request):
+     
+     return render(request, 'doctor.html')
+
+def contact(request):
+     return render(request, 'contact.html')
+
+def department(request):
+     
+    return render(request, 'department.html')
